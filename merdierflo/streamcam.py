23966495fbbec31929088c57100e832879479ae0
Floflo220 <f.vidal.c@outlook.fr>
2025-08-15 16:25:42 +0100

    Add files via upload

---
+++ merdierflo/streamcam.py
@@ -0,0 +1,30 @@
+from flask import Flask, Response
+import cv2
+
+
+
+app = Flask(__name__)
+
+cap = cv2.VideoCapture(2)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))  # pour éviter YUYV si ça bug
+
+def generate_frames():
+    while True:
+        success, frame = cap.read()
+        if not success:
+            continue
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame_bytes = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
